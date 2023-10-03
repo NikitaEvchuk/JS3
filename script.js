f22f00f24ec7856a46b614839185f8dc434b1225
NikitaEvchuk <144231215+NikitaEvchuk@users.noreply.github.com>
2023-10-03 19:57:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+/* let a = "Скільки"
+console.log(a.length)
+
+
+let b = "Today"
+console.log(b.toUpperCase())
+
+console.log(a.indexOf("и"))
+
+console.log(b.includes("today"))
+
+console.log(a.padEnd())
+
+let a1 = " Nikita "
+let a2 = 17 
+let a3 = "Yes"/* 
+console.log("My name is" + a1 + "I'm age " +a2 + " Is is cool weather today " + a3) */
+/* console.log(`My name is ${a1} Im age ${a2} Is it cool weather today ${a3}`)  */
+
+
+/* let name = "Nikita Evchuk"
+console.log(name.indexOf(" ")) */
+
+/* let name = "Nikita"
+let surname = "Evchuk"
+let time = "15:00"
+let position = 234
+console.log(`Гість  ${name} ${surname} посиляється в ${time} номер ${position}`) */
+
+
+
+/* Домашнє завдання */
+const result = 5+5+"5"
+console.log(result)
+console.log(typeof result)
+
+
+let email = "nikitaevcuk@gmail.com"
+console.log(email.includes("@"))
+console.log(email.length)
+
+let a1 = "My "
+let a2 = " name"
+let a3 = " is " 
+let fullName = a1 + a2 + a3 + "Victor"
+console.log(fullName)
+
+let useName = "Nikita"
+let payment = 300
+let mesege = alert(`Дякуємо, ${useName}! До сплати ${payment} гривень`)
